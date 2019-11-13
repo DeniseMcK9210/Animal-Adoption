42c45a8a4f216df536994d7445744ce6ab8ceb83
Denise McKinley <denise.m.mckinley@gmail.com>
2019-11-13 14:50:21 -0500

    data cleaning and map.JS
    initial map.js code
    fixed in/out df merge, matching intake and outcome occureences for each animal id

--- App/static/js/logic.js
+++ App/static/js/logic.js
@@ -1,86 +1,51 @@
-// Store our API endpoint inside queryUrl
-var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson"
+// Store our data endpoint inside queryUrl
+var queryUrl = "/aac"
 
 // Perform a GET request to the query URL
-d3.json(queryUrl, function(data) {
+d3.json(queryUrl).then(function(data){
   // Once we get a response, send the data.features object to the createFeatures function
-  createFeatures(data.features);
-});
+  createFeatures(data);
+}).catch(error => console.log(error));
 
 
 //Creates feature layer with API data input and completes map with map creator function
-function createFeatures(earthquakeData) {
-
-  // Define a function we want to run once for each feature for a pop up
-  function onEachFeature(feature, layer) {
-    layer.on({
-      // When a user's mouse touches a map feature, the mouseover event calls this function, that feature's opacity changes to 90% so that it stands out
-      mouseover: function(event) {
-        layer = event.target;
-        layer.setStyle({
-          fillOpacity: 1
-        });
-      },
-      // When the cursor no longer hovers over a map feature - when the mouseout event occurs - the feature's opacity reverts back to 50%
-      mouseout: function(event) {
-        layer = event.target;
-        layer.setStyle({
-          fillOpacity: 0.7
-        });
-      }
-    });
-
-    layer.bindPopup("<img src='" + img_url + "'><h3>" + name +
-      "</h3><hr><p> Age:" + age + "</p>" + "<p> Breed:"+ breed + "</p>"+"<p> Color:"+ color + "</p>"+"<p> Date Found:"+ found_date + "</p>");
-  }
+function createFeatures(adoptionData) {
 
   //Define a function to determine cicle color on earthquake magnitude
-  function chooseColor(magnitude) {
+  function chooseColor(aac) {
     switch (true) {
-    case magnitude <= 3.9:
+    case aac == "Yes (come to the shelter)":
       return "#FFEDA0"
-      break;
-    case magnitude > 3.9 && magnitude <= 4.9:
-      return "#FC4E2A"
-      break;
-    case magnitude > 4.9 && magnitude <= 5.9:
-      return "#BD0026"
-      break;
-    case magnitude > 5.9 && magnitude <= 6.9:
-      return "#7a0177"
-      break;
-    case magnitude > 6.9 && magnitude <= 7.9:
-      return "#78184a"
       break;
     default:
       return "#000";
     }
   }
 
-  // Create a GeoJSON layer containing the features array on the earthquakeData object
-  // Run the onEachFeature function once for each piece of data in the array
-  
-  var earthquakes = L.geoJSON(earthquakeData, {
-    pointToLayer: function (feature, latlng) {
-      return L.circleMarker(latlng, {
-        radius: feature.properties.mag ** 2 ,
-        fillColor: chooseColor(feature.properties.mag),
+  var animalMarkers = []
+  // Create a marker layer w/ pop up
+  //loop through adoptionData to create a marker for each row
+  adoptionData.forEach(function(data){
+    animalMarkers.push(
+      L.circleMarker([data.Latitude, data.Longitude], {
+        radius: 10,
+        fillColor: chooseColor(data['At AAC']),
         color: "#fff",
         fillOpacity: 0.7
-      });
-    },
-    onEachFeature: onEachFeature
-  });
-
+      }).bindPopup("<img src='" + data['Image Link'] + "'><h3>" + data['Intake Date']
+      + "</h3><hr><p> Age:" + data.Age + "</p><p> Breed:"+ data['Breed(unconfirmed)'] + 
+      "</p><p> Sex:"+ data.Found_Sex + data.Gender_x + "</p><p> Color:"+ data.color + "</p>")
+    );
+  })
+  
   // Sending our earthquakes layer to the createMap function
-  createMap(earthquakes);
+  createMap(animalMarkers);
 }
 
-
 //Map creator and settings function 
-function createMap(earthquakes) {
+function createMap(animals) {
 
-  // Define streetmap and darkmap layers
+  // Define streetmap layer
   var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
     maxZoom: 18,
@@ -88,26 +53,23 @@ function createMap(earthquakes) {
     accessToken: API_KEY
   });
 
-  // Create our map, giving it the streetmap and earthquakes layers to display on load
+  // Create our map, giving it the streetmap and animals layers to display on load
   var myMap = L.map("map", {
     center: [
-      29.26, 0.24
+      30.2672, 97.7431
     ],
-    zoom: 2,
-    layers: [streetmap, earthquakes]
+    zoom: 10,
+    layers: [streetmap, animals]
   });
 
+  //create Legend
   var legend = L.control({ position: "bottomleft" });
 
   legend.onAdd = function(map) {
     var div = L.DomUtil.create("div", "legend");
-    div.innerHTML += "<h4>Magnitude</h4>";
-    div.innerHTML += '<i style="background: #FFEDA0"></i><span>0-3.9</span><br>';
-    div.innerHTML += '<i style="background: #FC4E2A"></i><span>4-4.9</span><br>';
-    div.innerHTML += '<i style="background: #BD0026"></i><span>5-5.9</span><br>';
-    div.innerHTML += '<i style="background: #78184a"></i><span>6-6.9</span><br>';
-    div.innerHTML += '<i style="background: #4B0082"></i><span>7-7.9</span><br>';
-    div.innerHTML += '<i style="background: #000"></i><span>8+</span><br>';
+    div.innerHTML += "<h4>At AAC?</h4>";
+    div.innerHTML += '<i style="background: #FFEDA0"></i><span>Yes (come to the shelter)</span><br>';
+    div.innerHTML += '<i style="background: #000"></i><span>No (contact for more info)</span><br>';
     
     return div;
   };
